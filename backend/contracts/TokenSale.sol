591c383eb98e18694dabe389488b91a72f3981c8
sebastian <sebastianmensense0913@outlook.com>
2023-09-08 15:55:59 -0500

    add i_ prefix to all interfaces being used. add temp call to i_nftContract.requestNFt() in mintNft().

--- backend/contracts/TokenSale.sol
+++ backend/contracts/TokenSale.sol
@@ -10,30 +10,32 @@ import './interfaces/IGroupFiveCollection.sol';
 contract TokenSale is Ownable {
     uint256 public ratio;
     uint256 public price;
-    IGroupFiveToken public paymentToken;
-    IGroupFiveCollection public nftContract;
+    IGroupFiveToken public i_paymentToken;
+    IGroupFiveCollection public i_nftContract;
 
     constructor(uint256 _ratio, uint256 _price, address _paymentToken, address _nftContract) {
         ratio = _ratio;
         price = _price;
-        paymentToken = IGroupFiveToken(_paymentToken); // this makes paymentToken an interface
-        nftContract = IGroupFiveCollection(_nftContract); // this makes nftContract an interface
+        i_paymentToken = IGroupFiveToken(_paymentToken); // this makes i_paymentToken an interface
+        i_nftContract = IGroupFiveCollection(_nftContract); // this makes i_nftContract an interface
     }
 
     function buyTokens() external payable {
         uint256 amountToBeMinted = msg.value * ratio;
-        paymentToken.mint(msg.sender, amountToBeMinted); // in GroupFiveToken msg.sender will be the TokenSale contract itself
+        i_paymentToken.mint(msg.sender, amountToBeMinted); // in GroupFiveToken msg.sender will be the TokenSale contract itself
     }
 
     // function returnTokens(uint256 amount) external {
     //     // burn amount from sender
-    //     paymentToken.burnFrom(msg.sender, amount);
+    //     i_paymentToken.burnFrom(msg.sender, amount);
     //     // transfer eth to sender
     //     payable(msg.sender).transfer(amount / ratio);
     // }
 
-    function mintNft(uint256 nftId) external {
-        paymentToken.transferFrom(msg.sender, address(this), price);
-        // TODO mint nft
+    function mintNft() external {
+        // transfer payment of GFT from sender to TokenSale contract
+        i_paymentToken.transferFrom(msg.sender, address(this), price);
+        // mint nft
+        i_nftContract.requestNft()
     }
 }
