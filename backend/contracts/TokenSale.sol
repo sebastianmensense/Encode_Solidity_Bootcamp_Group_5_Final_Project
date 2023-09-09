d7b284c7597d2594dab2605295dc246647359b09
sebastian <sebastianmensense0913@outlook.com>
2023-09-09 06:13:49 -0500

    TokenSale.sol - add s_ prefix to storeage vars

--- backend/contracts/TokenSale.sol
+++ backend/contracts/TokenSale.sol
@@ -8,20 +8,20 @@ import './interfaces/IGroupFiveToken.sol';
 import './interfaces/IGroupFiveCollection.sol';
 
 contract TokenSale is Ownable {
-    uint256 public ratio;
-    uint256 public price;
+    uint256 public s_ratio;
+    uint256 public s_price;
     IGroupFiveToken public i_paymentToken;
     IGroupFiveCollection public i_nftContract;
 
     constructor(uint256 _ratio, uint256 _price, address _paymentToken, address _nftContract) {
-        ratio = _ratio;
-        price = _price;
+        s_ratio = _ratio;
+        s_price = _price;
         i_paymentToken = IGroupFiveToken(_paymentToken); // this makes i_paymentToken an interface
         i_nftContract = IGroupFiveCollection(_nftContract); // this makes i_nftContract an interface
     }
 
     function buyTokens() external payable {
-        uint256 amountToBeMinted = msg.value * ratio;
+        uint256 amountToBeMinted = msg.value * s_ratio;
         i_paymentToken.mint(msg.sender, amountToBeMinted); // in GroupFiveToken msg.sender will be the TokenSale contract itself
     }
 
@@ -39,7 +39,7 @@ contract TokenSale is Ownable {
 
     function mintNft() external {
         // transfer payment of GFT from sender to TokenSale contract
-        i_paymentToken.transferFrom(msg.sender, address(this), price);
+        i_paymentToken.transferFrom(msg.sender, address(this), s_price);
         // mint nft
         i_nftContract.requestNft()
     }
