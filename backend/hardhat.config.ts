04dcf879712cba49637b9d512a6ad86f319079aa
sebastian <sebastianmensense0913@outlook.com>
2023-09-09 15:48:01 -0500

    add solidity compiler version & prettier formattering

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,9 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  paths: { tests: "tests" },
-};
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.19',
+            },
+            {
+                version: '0.8.7',
+            },
+            {
+                version: '0.6.6',
+            },
+        ],
+    },
+    paths: { tests: 'tests' },
+}
 
-export default config;
+export default config
