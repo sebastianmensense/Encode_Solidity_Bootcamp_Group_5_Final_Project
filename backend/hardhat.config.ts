f8ee9b95167d4452df9cc37a646be1e6c8ac14b9
sebastian <sebastianmensense0913@outlook.com>
2023-09-10 17:07:57 -0500

    add imports, vars, and etherscan to hardhat.config to enable contract verification

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,14 @@
+import '@typechain/hardhat'
 import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-verify'
 import '@nomicfoundation/hardhat-toolbox'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const SEPOLIA_RPC_URL =
+    process.env.ALCHEMY_RPC_ENDPOINT_URL || 'https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -14,6 +23,9 @@ const config: HardhatUserConfig = {
                 version: '0.6.6',
             },
         ],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
     paths: { tests: 'tests' },
 }
