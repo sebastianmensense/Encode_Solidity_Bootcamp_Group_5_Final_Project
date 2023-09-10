f1d23c9a60bdb1f7609fc303ac6220bd328a26d1
sebastian <sebastianmensense0913@outlook.com>
2023-09-10 17:22:24 -0500

    add NFT metadata into constants dir

---
+++ backend/constants/nftImageIPFSHashes.md
@@ -0,0 +1,16 @@
+# Group Five Collection NFT Image IPFS Hashes
+
+Dragon:
+ipfs://QmT2UhRCqsQ6SRejeSEHzPs7w4VA4zQzoN4oZLhgTzvMcr
+
+Knight:
+ipfs://QmWytKnnfSqZDjrrDgBkdFPBSfx5qskZCQuNmFco7NqYTY
+
+Scarecrow:
+ipfs://QmfUxkRC2XdDEnJxUKrNjfvjL3oN4mWxjJqAH8AVnousZ9
+
+Squire:
+ipfs://QmPuvS64GdMGXE4zUSKeF5xspUJutwKN1uKdnWnuxjvTf8
+
+Werewolf:
+ipfs://QmUPS6QAyUZgKvm34DqJFz8HjjoSCqatNjpveQ5eDDcEcu
---
+++ backend/constants/nftMetadata.ts
@@ -0,0 +1,118 @@
+const nftPower1Metadata = {
+    name: 'Scarecrow',
+    description: `
+    A very common, very weak, scarecrow
+
+    Chance of being minted: 50%
+ 
+
+    An NFT from the Group Five Collection (GFC).
+
+    This NFT can be used in the Encode Battle game
+
+    in order to earn GFT
+    `,
+    image: 'ipfs://QmfUxkRC2XdDEnJxUKrNjfvjL3oN4mWxjJqAH8AVnousZ9',
+    attributes: [
+        {
+            trait_type: 'Power',
+            value: 1,
+        },
+    ],
+}
+const nftPower2Metadata = {
+    name: 'Squire',
+    description: `
+    A common young squire
+
+    Chance of being minted: 20%
+
+    
+    An NFT from the Group Five Collection (GFC)
+
+    This NFT can be used in the Encode Battle game
+
+    in order to earn GFT
+    `,
+    image: 'ipfs://QmPuvS64GdMGXE4zUSKeF5xspUJutwKN1uKdnWnuxjvTf8',
+    attributes: [
+        {
+            trait_type: 'Power',
+            value: 2,
+        },
+    ],
+}
+const nftPower3Metadata = {
+    name: 'Knight',
+    description: `
+    An uncommon veteran knight
+
+    Chance of being minted: 15%
+
+    
+    An NFT from the Group Five Collection (GFC)
+
+    This NFT can be used in the Encode Battle game
+
+    in order to earn GFT
+    `,
+    image: 'ipfs://QmWytKnnfSqZDjrrDgBkdFPBSfx5qskZCQuNmFco7NqYTY',
+    attributes: [
+        {
+            trait_type: 'Power',
+            value: 3,
+        },
+    ],
+}
+const nftPower4Metadata = {
+    name: 'Werewolf',
+    description: `
+    A rare savage werewolf
+
+    Chance of being minted: 10%
+
+    
+    An NFT from the Group Five Collection (GFC)
+
+    This NFT can be used in the Encode Battle game
+
+    in order to earn GFT
+    `,
+    image: 'ipfs://QmUPS6QAyUZgKvm34DqJFz8HjjoSCqatNjpveQ5eDDcEcu',
+    attributes: [
+        {
+            trait_type: 'Power',
+            value: 4,
+        },
+    ],
+}
+const nftPower5Metadata = {
+    name: 'Dragon',
+    description: `
+    A very rare monstrous dragon
+
+    Chance of being minted: 5%
+
+    
+    An NFT from the Group Five Collection (GFC)
+
+    This NFT can be used in the Encode Battle game
+    
+    in order to earn GFT
+    `,
+    image: 'ipfs://QmT2UhRCqsQ6SRejeSEHzPs7w4VA4zQzoN4oZLhgTzvMcr',
+    attributes: [
+        {
+            trait_type: 'Power',
+            value: 5,
+        },
+    ],
+}
+
+export const nftMetadata = [
+    nftPower1Metadata,
+    nftPower2Metadata,
+    nftPower3Metadata,
+    nftPower4Metadata,
+    nftPower5Metadata,
+]
