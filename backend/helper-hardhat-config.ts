95fb39c0b747480c489bc53d5bb50b6849940d03
sebastian <sebastianmensense0913@outlook.com>
2023-09-10 17:06:07 -0500

    add VRF subscriptionId to helper-hardhat-config

--- backend/helper-hardhat-config.ts
+++ backend/helper-hardhat-config.ts
@@ -31,8 +31,7 @@ export const networkConfig: networkConfigInfo = {
         vrfCoordinatorV2: '0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625',
         gasLane: '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c',
         callbackGasLimit: '500000', // 500,000 gas
-        mintFee: '10000000000000000', // 0.01 ETH
-        subscriptionId: '2983', // add your ID here!
+        subscriptionId: '2983',
     },
 }
 
