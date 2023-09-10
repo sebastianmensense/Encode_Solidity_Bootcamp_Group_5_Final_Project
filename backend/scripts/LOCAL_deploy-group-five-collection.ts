3eae5e227146b7b39e0c112d6449209bf0a54d8b
sebastian <sebastianmensense0913@outlook.com>
2023-09-10 17:12:08 -0500

    LOCAL_deploy-group-five-collection.ts - remove unused code

--- backend/scripts/LOCAL_deploy-group-five-collection.ts
+++ backend/scripts/LOCAL_deploy-group-five-collection.ts
@@ -12,7 +12,6 @@ dotenv.config()
 const BASE_FEE = '250000000000000000' // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
 const FUND_AMOUNT = '1000000000000000000000'
-const POWER_LEVEL_VARIANTS = 5
 
 type tokenUriArray = [string, string, string, string, string]
 
