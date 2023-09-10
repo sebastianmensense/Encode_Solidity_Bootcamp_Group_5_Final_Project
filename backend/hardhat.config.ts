a93b50c49cbf440fd930dfa11350ba23fdb52c7f
sebastian <sebastianmensense0913@outlook.com>
2023-09-10 17:08:11 -0500

    add networks to hardhat.config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -11,6 +11,18 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+    },
     solidity: {
         compilers: [
             {
