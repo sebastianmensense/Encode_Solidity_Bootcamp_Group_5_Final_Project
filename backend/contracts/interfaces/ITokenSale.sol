be56aa0662002bfcf6bd893118bc376eba489510
sebastian <sebastianmensense0913@outlook.com>
2023-09-11 12:38:46 -0500

    ITokenSale.sol - update interface

--- backend/contracts/interfaces/ITokenSale.sol
+++ backend/contracts/interfaces/ITokenSale.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 interface ITokenSale {
     function buyTokens() external payable;
 
+    // function approveTokenSale() external;
+
     function mintNft(uint256 nftId) external;
 }
