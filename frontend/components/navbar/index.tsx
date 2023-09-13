f8997d02120bef43468e7a4ef8901bdb560fb078
sebastian <sebastianmensense0913@outlook.com>
2023-09-13 12:48:46 -0500

    edit navbar

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,15 +1,11 @@
-
 'use client'
 
-import { ConnectKitButton } from "connectkit";
-import styles from "./Navbar.module.css";
+import { ConnectKitButton } from 'connectkit'
+import styles from './navbar.module.css'
 export default function Navbar() {
-  return (
-    <nav className={styles.navbar}>
-      <a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-        <p>create-web3-dapp</p>
-      </a>
-      <ConnectKitButton />
-    </nav>
-  );
+    return (
+        <nav className={styles.navbar}>
+            <ConnectKitButton />
+        </nav>
+    )
 }
