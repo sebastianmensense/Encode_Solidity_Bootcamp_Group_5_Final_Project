c7736ddd978aa6c9c94411268848e97870d3e9c6
sebastian <sebastianmensense0913@outlook.com>
2023-09-13 12:46:27 -0500

    add AppContainer component and styles

---
+++ frontend/components/AppContainer/index.tsx
@@ -0,0 +1,66 @@
+import appStateOptions from '@/constants/appStateOptions'
+import styles from './appContainer.module.css'
+import Home from '../Home'
+import BuyToken from '../BuyToken'
+import BuyNft from '../BuyNft'
+import Battle from '../Battle'
+import { useAccount, useNetwork } from 'wagmi'
+
+type Props = {
+    appState: string
+    changeAppState: React.Dispatch<React.SetStateAction<string>>
+}
+
+const HOME = appStateOptions.home
+const BUY_TOKEN = appStateOptions.buyToken
+const BUY_NFT = appStateOptions.buyNft
+const BATTLE = appStateOptions.battle
+
+export default function AppContainer({ appState, changeAppState }: Props) {
+    const { address, isConnecting, isDisconnected } = useAccount()
+    const { chain } = useNetwork()
+    if (address)
+        return (
+            <div className={styles.container}>
+                {appState === HOME && <Home changeAppState={changeAppState}></Home>}
+                {appState === BUY_TOKEN && (
+                    <BuyToken
+                        changeAppState={changeAppState}
+                        address={address}
+                        chain={chain}
+                    ></BuyToken>
+                )}
+                {appState === BUY_NFT && (
+                    <BuyNft
+                        changeAppState={changeAppState}
+                        address={address}
+                        chain={chain}
+                    ></BuyNft>
+                )}
+                {appState === BATTLE && (
+                    <Battle
+                        changeAppState={changeAppState}
+                        address={address}
+                        chain={chain}
+                    ></Battle>
+                )}
+            </div>
+        )
+    if (isConnecting)
+        return (
+            <div className={styles.container}>
+                <h2> Loading Wallet Info...</h2>
+            </div>
+        )
+    // if (isDisconnected)
+    //     return (
+    //         <div className={styles.container}>
+    //             <h2>Wallet Disconnected</h2>
+    //         </div>
+    //     )
+    return (
+        <div className={styles.container}>
+            <h2>Please connect your wallet</h2>
+        </div>
+    )
+}
