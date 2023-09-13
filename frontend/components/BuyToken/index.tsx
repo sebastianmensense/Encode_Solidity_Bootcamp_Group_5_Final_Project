0ba3a35d2340d23c9409dde853614bc86cdc542e
sebastian <sebastianmensense0913@outlook.com>
2023-09-13 16:45:42 -0500

    update BuyToken component and styles

--- frontend/components/BuyToken/index.tsx
+++ frontend/components/BuyToken/index.tsx
@@ -2,7 +2,19 @@ import { Button, Input } from '@web3uikit/core'
 import appStateOptions from '@/constants/appStateOptions'
 import styles from './buyToken.module.css'
 import { useState } from 'react'
-import { Chain } from 'wagmi'
+import {
+    Chain,
+    sepolia,
+    useWaitForTransaction,
+    useBalance,
+    usePrepareSendTransaction,
+    useSendTransaction,
+    usePrepareContractWrite,
+    useContractWrite,
+} from 'wagmi'
+import { parseEther } from 'viem'
+import * as tokenSaleJson from '@/assets/TokenSale.json'
+import { useDebounce } from '@/hooks/useDebounce'
 
 type Props = {
     changeAppState: React.Dispatch<React.SetStateAction<string>>
@@ -15,20 +27,72 @@ type Props = {
 }
 
 const HOME = appStateOptions.home
+const tokenSaleAbi = tokenSaleJson.abi
 
 export default function BuyToken({ changeAppState, address, chain }: Props) {
-    const [ethBalance, setEthBalance] = useState(0)
-    const [gftBalance, setgftBalance] = useState(0)
     const [ethInput, setEthInput] = useState(0)
     const [gftInput, setGftInput] = useState(0)
 
+    const debouncedEthInput = useDebounce(ethInput)
+
     function calculateEthToGft(value: string): void {
+        setEthInput(Number(value))
         setGftInput(Number(value) * 100)
+        // refetch().catch((e) => console.error('refetch error', e))
     }
 
     function calculateGftToEth(value: string): void {
         setEthInput(Number(value) / 100)
+        setGftInput(Number(value))
+        // refetch().catch((e) => console.error('refetch error', e))
     }
+
+    function handleSubmit(e: any) {
+        e.preventDefault()
+        console.log('ethInput: ', ethInput)
+        write?.()
+    }
+
+    const {
+        data: gftData,
+        isError: gftBalanceError,
+        isLoading: gftBalanceLoading,
+    } = useBalance({
+        address: address,
+        token: process.env.NEXT_PUBLIC_GROUP_FIVE_TOKEN as `0x${string}`,
+    })
+
+    const {
+        data: ethData,
+        isError: ethBalanceError,
+        isLoading: ethBalanceLoading,
+    } = useBalance({
+        address: address,
+    })
+
+    const { config } = usePrepareContractWrite({
+        address: (process.env.NEXT_PUBLIC_TOKEN_SALE as `0x${string}`) ?? '',
+        abi: tokenSaleAbi,
+        functionName: 'buyTokens',
+        chainId: sepolia.id,
+        account: address as `0x${string}`,
+        value: parseEther(`${debouncedEthInput}`),
+        onError(error) {
+            console.log('usePrepare Purchase Token Error: ', error)
+        },
+        onSettled(data) {
+            console.log('usePrepare Purchase Token Success: ', data)
+        },
+    })
+
+    const { data, isLoading: loadingWriteTx, error, isError, write } = useContractWrite(config)
+    const { isLoading: awaitingTx, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
+
+    // console.log('awaitingTX: ', awaitingTX)
+    // console.log('sendTransaction double banged: ', !!sendTransaction)
+    // console.log('sendTransaction: ', sendTransaction)
 
     return (
         <div className={styles.container}>
@@ -50,15 +114,19 @@ export default function BuyToken({ changeAppState, address, chain }: Props) {
             <div className={styles.actionContainer}>
                 <p>Your Balance</p>
                 <div className={styles.balanceContainer}>
-                    <p>ETH: {ethBalance}</p>
-                    <p>GFT: {gftBalance}</p>
+                    <p>SEP: {ethData?.formatted.slice(0, 5)}</p>
+                    <p>GFT: {gftData?.formatted.slice(0, 5)}</p>
                 </div>
-                <div className={styles.calculation}>
+                <form
+                    className={styles.form}
+                    onSubmit={(e) => {
+                        handleSubmit(e)
+                    }}
+                >
                     <Input
                         id="eth-input"
                         label="ETH"
                         name="eth-input"
-                        onBlur={function noRefCheck() {}}
                         onChange={(e) => calculateEthToGft(e.target.value)}
                         placeholder="0.01"
                         type="number"
@@ -69,7 +137,6 @@ export default function BuyToken({ changeAppState, address, chain }: Props) {
                         id="gft-input"
                         label="GFT"
                         name="gft-input"
-                        onBlur={function noRefCheck() {}}
                         onChange={(e) => calculateGftToEth(e.target.value)}
                         placeholder="0.01"
                         type="number"
@@ -78,15 +145,34 @@ export default function BuyToken({ changeAppState, address, chain }: Props) {
                     />
                     <Button
                         isFullWidth
+                        // true === disabled
+                        // false === enabled
+                        disabled={!write || awaitingTx || loadingWriteTx || ethInput <= 0}
+                        // disabled={!write | awaitingTX || loadingWriteTx}
                         color="blue"
                         id=""
-                        onClick={() => changeAppState(HOME)}
                         size="large"
-                        text="Purchase GFT"
+                        text={awaitingTx || loadingWriteTx ? 'Please wait...' : 'Purchase GFT'}
                         theme="colored"
-                        type="button"
+                        type="submit"
                     />
-                </div>
+                </form>
+                {loadingWriteTx && <div>Sign Transaction...</div>}
+                {awaitingTx && <div>Waiting for Transaction...</div>}
+                {isSuccess && (
+                    <div>
+                        <p>Purchase Succesful!</p>
+                        <div>
+                            <a
+                                target="_blank"
+                                href={`https://sepolia.etherscan.io/tx/${data?.hash}`}
+                            >
+                                View On Etherscan
+                            </a>
+                        </div>
+                    </div>
+                )}
+                {isError && <div>Error: {error?.message}</div>}
             </div>
         </div>
     )
