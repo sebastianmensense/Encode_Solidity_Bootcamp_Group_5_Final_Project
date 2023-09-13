fb5a25ae9451b1c051aea5325129c84766ed7635
sebastian <sebastianmensense0913@outlook.com>
2023-09-13 12:45:10 -0500

    add Home component and styles

---
+++ frontend/components/Home/index.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@web3uikit/core'
+import styles from './home.module.css'
+import appStateOptions from '@/constants/appStateOptions'
+
+type Props = {
+    changeAppState: React.Dispatch<React.SetStateAction<string>>
+}
+
+const HOME = appStateOptions.home
+const BUY_TOKEN = appStateOptions.buyToken
+const BUY_NFT = appStateOptions.buyNft
+const BATTLE = appStateOptions.battle
+
+export default function Home({ changeAppState }: Props) {
+    return (
+        <div className={styles.container}>
+            <h2>Home</h2>
+            <div className={styles.buttonContainer}>
+                <Button
+                    color="blue"
+                    id=""
+                    isFullWidth
+                    onClick={() => changeAppState(BUY_TOKEN)}
+                    size="large"
+                    text="Buy Token"
+                    theme="colored"
+                    type="button"
+                />
+                <Button
+                    color="blue"
+                    id=""
+                    isFullWidth
+                    onClick={() => changeAppState(BUY_NFT)}
+                    size="large"
+                    text="Buy Nft"
+                    theme="colored"
+                    type="button"
+                />
+                <Button
+                    color="blue"
+                    id=""
+                    isFullWidth
+                    onClick={() => changeAppState(BATTLE)}
+                    size="large"
+                    text="Battle"
+                    theme="colored"
+                    type="button"
+                />
+            </div>
+        </div>
+    )
+}
